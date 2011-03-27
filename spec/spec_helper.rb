13d84d8711494227d25feca1d1491f15dd504dc6
Ezekiel Templin <ezkl@me.com>
2011-03-27 07:18:42 -0400

    Cleaning up support requires.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,9 @@
 # encoding: UTF-8
 require 'simplecov'
-
 SimpleCov.start do
   coverage_dir 'coverage'
   add_filter 'spec/'
 end
 
-require 'rspec'
-require 'support/with_constants'
-require 'support/deferred_garbage_collection' # http://makandra.com/notes/950-speed-up-rspec-by-deferring-garbage-collection
-require 'support/temporary_folder_helper'
-
+Dir[File.expand_path('support', File.dirname(__FILE__)) + "/**/*.rb"].each { |f| require f }
 require 'capit'
